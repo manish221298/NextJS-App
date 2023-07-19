dd7343cf676596b826bed67dd4da6aa4c0422f01
manish.kumar <manish.kumar@kochartech.com>
2023-07-19 12:02:07 +0530

    make helper api

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,27 @@
-export default function ProfilePage(){
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1>Profile Pages</h1>
-            <h2>Manish Kumar</h2>
-        </div>
-    )
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function ProfilePage() {
+  const [data, setData] = useState("nothing");
+
+  async function getUserData() {
+    const res = await axios.get("/api/users/me");
+    console.log("res.data", res.data.data);
+    setData(res?.data?.data);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1>Profile Pages</h1>
+      <h2>Manish Kumar</h2>
+      <button
+        className="bg-blue-500 hover: bg-blue-700 text-white font-bold py-2 px-3 rounded"
+        onClick={getUserData}
+      >
+        clickme
+      </button>
+    </div>
+  );
 }
